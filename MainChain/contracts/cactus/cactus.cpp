f361c11304553146c2a26bd6cc8e98e13cb4fd4e
“xueyh” <xyh393@126.com>
2018-08-04 11:32:54 +0800

    删除debug信息 by xueyh

--- MainChain/contracts/cactus/cactus.cpp
+++ MainChain/contracts/cactus/cactus.cpp
@@ -18,7 +18,7 @@ class cactus : public eosio::contract {
       cts(_self, _self){}
       //@abi action 
       void transfer(account_name from,asset quantity) {
-         print( "Hello, ", name{from} );
+         //print( "Hello, ", name{from} );
          auto quant_after_fee = quantity;
          
          eosio_assert( is_account( from ), "to account does not exist");
@@ -26,37 +26,21 @@ class cactus : public eosio::contract {
 
          eosio_assert( quantity.is_valid(), "invalid quantity" );
          eosio_assert( quantity.amount > 0, "must withdraw positive quantity" );
-         printf("%s\n", "开始校验from的权限");
+         //printf("%s\n", "开始校验from的权限");
 
-         require_auth(_self);
-         printf("%s\n", "开始转from的账");
+         require_auth(from);
+         //printf("%s\n", "开始转from的账");
          action(
               permission_level{ from, N(active) },
               N(eosio.token), N(transfer),
               std::make_tuple(from,_self, quantity, std::string("cactus transfer"))
             ).send();
 
-         /*INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {payer,N(active)},
-         { payer, N(eosio.ram), quant_after_fee, std::string("buy ram") } );*/
-
-        //cts_his_index cts(_self, _self);
-
-
-
-
-
-
-         /* uint64_t tmp_id =0;
-         if(itr!=cts.end()){
-            tmp_id = itr->id+1;
-         }*/
-
          cts.emplace(_self, [&](auto& a){
                   a.id = cts.available_primary_key();;
                   a.from      = from;
                   a.amount = quantity.amount;
-                  // a.creation_date = eosio::time_point_sec();
-                  a.creation_date = eosio::time_point();
+                  a.creation_date = eosio::time_point_sec();
                });
 
          //printf("%s\n", "执行cactus transfer 结束，请确认到账情况");
@@ -75,7 +59,7 @@ class cactus : public eosio::contract {
 
         
 
-         printf("%s\n", "执行cactus tmp 结束，请确认到账情况");
+         //printf("%s\n", "执行cactus tmp 结束，请确认到账情况");
       }
 
     private:
